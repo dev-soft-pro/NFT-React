ced82cb9f8372e7367f3aea73fc4d1a8c94e4bfe
CF <pclienth@hotmail.com>
2021-11-30 09:12:29 +0800

    Faq finish

--- src/pages/faq/index.js
+++ src/pages/faq/index.js
@@ -1,56 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DefaultPage from '../../components/DefaultPage/DefaultPage'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlusSquare } from '@fortawesome/free-regular-svg-icons'
+import { faPlusSquare, faMinusSquare } from '@fortawesome/free-regular-svg-icons'
 
 import './faq.css'
 
 export default function Faq(props) {
+  const [qShow1, setQShow1] = useState(false);
+  const [qShow2, setQShow2] = useState(false);
+  const [qShow3, setQShow3] = useState(false);
+  const [qShow4, setQShow4] = useState(false);
+  const [qShow5, setQShow5] = useState(false);
   return (
     <DefaultPage>
       <div className="page-home">
         <p className="text-magento-border" style={{ marginTop: '2vh' }}>FAQ</p>
         <div className="faq-text-group">
-          <div className="faq-text-question">
-            <FontAwesomeIcon icon={faPlusSquare} className="faq-text-plus" />
+          <div className="faq-text-question" onClick={() => setQShow1(prev => !prev)}>
+            <FontAwesomeIcon icon={qShow1 ? faMinusSquare : faPlusSquare} className="faq-text-plus" />
             <p className="text-faq-black">How do I download MetaMask and fund my MetaMask wallet?</p>
           </div>
-          <p className="faq-text-answer">
-            1. You need to download MetaMask. <br />
-            2. Buy Ethereum from an exchange. <br />
-            3. Transfer it to your MetaMask Wallet. <br />
-          </p>
-          <div className="faq-text-question">
-            <FontAwesomeIcon icon={faPlusSquare} className="faq-text-plus" />
+          {qShow1 && (
+            <p className="faq-text-answer">
+              1. You need to download MetaMask. <br />
+              2. Buy Ethereum from an exchange. <br />
+              3. Transfer it to your MetaMask Wallet. <br />
+            </p>
+          )}
+          <div className="faq-text-question" onClick={() => setQShow2(prev => !prev)}>
+            <FontAwesomeIcon icon={qShow2 ? faMinusSquare : faPlusSquare} className="faq-text-plus" />
             <p className="text-faq-black">How do I mint an NFT?</p>
           </div>
-          <p className="faq-text-answer">
-            Once you have MetaMask installed and Ethereum in your wallet you can click here to mint an NFT.
-          </p>
-          <div className="faq-text-question">
-            <FontAwesomeIcon icon={faPlusSquare} className="faq-text-plus" />
+          {qShow2 && (
+            <p className="faq-text-answer">
+              Once you have MetaMask installed and Ethereum in your wallet you can click here to mint an NFT.
+            </p>
+          )}
+          <div className="faq-text-question" onClick={() => setQShow3(prev => !prev)}>
+            <FontAwesomeIcon icon={qShow3 ? faMinusSquare : faPlusSquare} className="faq-text-plus" />
             <p className="text-faq-black">When can I mint an NFT?</p>
           </div>
-          <p className="faq-text-answer">
-            You can officially mint an NFT on January 8th at 1:00 pm ET.
-          </p>
-          <div className="faq-text-question">
-            <FontAwesomeIcon icon={faPlusSquare} className="faq-text-plus" />
+          {qShow3 && (
+            <p className="faq-text-answer">
+              You can officially mint an NFT on January 8th at 1:00 pm ET.
+            </p>
+          )}
+          <div className="faq-text-question" onClick={() => setQShow4(prev => !prev)}>
+            <FontAwesomeIcon icon={qShow4 ? faMinusSquare : faPlusSquare} className="faq-text-plus" />
             <p className="text-faq-black">What do you have planned for the future of CryptoHermits?</p>
           </div>
-          <p className="faq-text-answer">
-            We have a lot planned once we reach 100% in sales for the bookworm collection. 
-            The next collection we are excited for is the homesteader collection. 
-            From there we hope to expand to other collections. 
-            We want to create a community of self-sufficient, independent thinkers who aren't afraid to fight censorship!
-          </p>
-          <div className="faq-text-question">
-            <FontAwesomeIcon icon={faPlusSquare} className="faq-text-plus" />
+          {qShow4 && (
+            <p className="faq-text-answer">
+              We have a lot planned once we reach 100% in sales for the bookworm collection. 
+              The next collection we are excited for is the homesteader collection. 
+              From there we hope to expand to other collections. 
+              We want to create a community of self-sufficient, independent thinkers who aren't afraid to fight censorship!
+            </p>
+          )}
+          <div className="faq-text-question" onClick={() => setQShow5(prev => !prev)}>
+            <FontAwesomeIcon icon={qShow5 ? faMinusSquare : faPlusSquare} className="faq-text-plus" />
             <p className="text-faq-black">Can I buy on my mobile phone?</p>
           </div>
-          <p className="faq-text-answer">
-            Yes, simply download MetaMask for iOS or Android and visit our site using the MetaMask browser.
-          </p>
+          {qShow5 && (
+            <p className="faq-text-answer">
+              Yes, simply download MetaMask for iOS or Android and visit our site using the MetaMask browser.
+            </p>
+          )}
         </div>
       </div>
     </DefaultPage>
